31d3d6c2eb51e559902869c066381f8a291b09ca
hurnhu <hurnhu5622@gmail.com>
2018-06-18 15:38:13 +0200

    fix

--- reCount/hurnhu/recount.cpp
+++ reCount/hurnhu/recount.cpp
@@ -7,32 +7,31 @@
 using namespace std;
 template <typename T1, typename T2>
 struct greater_second {
-		typedef pair<T1, T2> type;
-			bool operator ()(type const& a, type const& b) const {
-						return a.second > b.second;
-							}
+	typedef pair<T1, T2> type;
+	bool operator ()(type const& a, type const& b) const {
+		return a.second > b.second;
+	}
 };
 int main() {
-		map<string, int> poll;
-			string name;
-				while (getline(cin, name))
-						{
-									if (name == "***")
-												{
-																break;
-																		}
-											poll[name]++;
-
-												}
-						vector<pair<string, int>> mapcopy(poll.begin(), poll.end());
-								sort(mapcopy.begin(), mapcopy.end(), greater_second<string, int>());
-										if (mapcopy[0].second == mapcopy[1].second)
-													{
-																	cout << "Runoff!" << endl;
-																			}
-												else
-															{
-																			cout << mapcopy[0].first << endl;
-																					}
-														return 0;
+	map<string, int> poll;
+	string name;
+	while (getline(cin, name))
+	{
+		if (name == "***")
+		{
+			break;
+		}
+		poll[name]++;
+	}
+	vector<pair<string, int>> mapcopy(poll.begin(), poll.end());
+	sort(mapcopy.begin(), mapcopy.end(), greater_second<string, int>());
+	if (mapcopy[0].second == mapcopy[1].second)
+	{
+		cout << "Runoff!" << endl;
+	}
+	else
+	{
+		cout << mapcopy[0].first << endl;
+	}
+	return 0;
 }
